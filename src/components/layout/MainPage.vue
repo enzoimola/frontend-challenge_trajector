5a2aaa4ddb98953ba33df3f3898990e19e884d50
enzoimola <ennzoimola@hotmail.com>
2023-06-05 22:07:46 -0300

    Initial commit

---
+++ src/components/layout/MainPage.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h1>Welcome to the Main Page!</h1>
+    <p>This is the content of the main page.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  components: {
+
+  },
+};
+</script>
